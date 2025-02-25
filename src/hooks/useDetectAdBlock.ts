d09ed23791f400fca6067c5685cdcf781c89f54a
litc0de <litc0de@posteo.org>
2025-02-25 16:01:57 +0100

    fix naming and add comments

--- src/hooks/useDetectAdBlock.ts
+++ src/hooks/useDetectAdBlock.ts
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Detect if user is using an adblock extension
+ * @see https://www.detectadblock.com/
+ * @see https://stackoverflow.com/questions/4869154/how-to-detect-adblock-on-my-website
+ * @see https://html-online.com/articles/how-to-detect-adblockers-with-javascript/
+ * @see https://tms-outsource.com/blog/posts/how-do-websites-detect-adblock/
+ * @see https://www.geeksforgeeks.org/how-to-detect-ad-blocker-using-mutation-observer-api/
+ */
 export const useDetectAdblock = () => {
   const [adBlockDetected, setAdBlockDetected] = useState(false);
 
