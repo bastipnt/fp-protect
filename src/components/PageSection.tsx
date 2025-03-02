7402224a4b885ce1005a71a26d73aee14df6794b
litc0de <litc0de@posteo.org>
2025-03-02 16:28:37 +0100

    remove border-y from page section

--- src/components/PageSection.tsx
+++ src/components/PageSection.tsx
@@ -13,7 +13,7 @@ const PageSection: React.FC<Props> = ({ children, bg, referenceIds, id }) => {
   return (
     <section
       id={id}
-      className={`${bg ? "bg-primary border-y-4 border-dashed" : ""} flex w-screen flex-col items-center py-8 sm:py-12`}
+      className={`${bg ? "bg-primary" : ""} flex w-screen flex-col items-center py-8 sm:py-12`}
     >
       <div className="flex w-150 max-w-screen flex-col gap-4 px-4 sm:px-8">
         {children}
