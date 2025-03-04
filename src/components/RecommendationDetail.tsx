41f7ad42c76748243ddaf9b200e34b6f835d116c
litc0de <litc0de@posteo.org>
2025-03-04 14:39:41 +0100

    add translations for recommendations

--- src/components/RecommendationDetail.tsx
+++ src/components/RecommendationDetail.tsx
@@ -1,21 +1,24 @@
+import { useTranslation } from "react-i18next";
 import ImgLink from "./ImgLink";
 
 type Props = {
   title: string;
   href: string;
   imgUrl: string;
-  description: string;
+  description: { [language: string]: string };
   bg?: boolean;
   wide?: boolean;
 };
 
 const RecommendationDetail: React.FC<Props> = ({ title, href, imgUrl, bg, description, wide }) => {
+  const { i18n } = useTranslation();
+
   return (
     <div
       className={`grid w-full grid-cols-[100px_1fr] items-center gap-4 sm:grid-cols-[120px_1fr]`}
     >
       <ImgLink href={href} imgUrl={imgUrl} title={title} bg={bg} wide={wide} small />
-      <p>{description}</p>
+      <p>{description[i18n.resolvedLanguage || "en"]}</p>
     </div>
   );
 };
