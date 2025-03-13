6d617f2950f9c3018d6e95fd0b677960fb9a66fc
litc0de <litc0de@posteo.org>
2025-03-13 21:22:55 +0100

    fix safari links

--- src/components/ReferenceList.tsx
+++ src/components/ReferenceList.tsx
@@ -9,16 +9,13 @@ const ReferenceList: React.FC<Props> = ({ referenceIds }) => {
   return (
     <>
       {referenceIds.length > 0 && (
-        <ol className="text-stroke-light mt-4 grid grid-cols-[auto_1fr] text-xs sm:ml-0">
+        <ol className="text-stroke-light mt-4 grid grid-cols-[auto_1fr] overflow-y-scroll text-xs sm:ml-0">
           {referenceIds.map((referenceId) => {
             const { url } = references[referenceId];
             const index = getIndex(referenceId);
 
             return (
-              <li
-                key={`${referenceId}`}
-                className="col-span-2 grid grid-cols-subgrid flex-row gap-1"
-              >
+              <li key={`${referenceId}`} className="col-span-2 grid grid-cols-subgrid gap-1">
                 <small className="mt-0.5">{index}. </small>
                 <a
                   className="cursor-pointer underline"
